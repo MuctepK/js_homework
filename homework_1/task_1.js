8eec627ebb97980d118919263f22e1caf914eaa7
Kutman <muctep.k@gmail.com>
2019-11-20 10:47:32 +0600

    Закомментировал ненужный код

--- homework_1/task_1.js
+++ homework_1/task_1.js
@@ -1,14 +1,13 @@
 ar = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко'];
-for (let index in ar) {
-    console.log(ar[index]);
-    if (ar[index] === 'молоко')
+for (let value of ar) {
+    if (value === 'молоко')
         console.log('хорошо');
-    else if (ar[index] === 'пиво')
+    else if (value === 'пиво')
         console.log('плохо');
 }
-ar.forEach((value) => {
-        if (value==='молоко')
-            console.log('хорошо');
-        else if (value==='пиво')
-            console.log('плохо');
-        });
+// ar.forEach((value) => {
+//         if (value==='молоко')
+//             console.log('хорошо');
+//         else if (value==='пиво')
+//             console.log('плохо');
+//         });
